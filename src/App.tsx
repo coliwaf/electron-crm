acc58b8240d1943dfbc59073ad068326026a1078
Collins Wafula <coliwafula@gmail.com>
2025-06-16 12:26:53 +0300

    successfully installed tailwind v4 on webpack

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 function Homepage() {
     return(
         <div>
-            <h1>Hello react in electron</h1>
-            <h1>Homepage</h1>
+          <h1 className="font-bold text-2xl underline text-red-700">Hello react</h1>
+            <h1>Hello Abrit react in electron!</h1>
+            <p>Homepage</p>
         </div>
     )
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -29,4 +29,4 @@
 import './index.css';
 import './Main' // <== add this line
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+// console.log('👋 This message is being logged by "renderer.js", included via webpack');
